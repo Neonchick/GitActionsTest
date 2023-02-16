2a06a61c312db7557481bfedd4e4d8dff1f7ccc8
Neonchick <57732664+Neonchick@users.noreply.github.com>
2023-02-16 15:32:39 +0100

    change package

--- src/main/java/com/neonchick/gitactionstest/GitActionsTestApplication.java
+++ src/main/java/com/neonchick/gitactionstest/GitActionsTestApplication.java
@@ -1,4 +1,4 @@
-package com.example.gitactionstest;
+package com.neonchick.gitactionstest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
