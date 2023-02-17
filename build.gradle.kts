7435c0ff263c4f36589f5bc85007a195b5999ef9
Neonchick <57732664+Neonchick@users.noreply.github.com>
2023-02-17 21:20:13 +0100

    add itself dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,16 @@ version = "0.0.1-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/Neonchick/GitActionsTest")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
 }
 
 publishing {
@@ -29,6 +39,7 @@ publishing {
 }
 
 dependencies {
+    implementation("org.example:github-actions-test:0.0.1-SNAPSHOT")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
